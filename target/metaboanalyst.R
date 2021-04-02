240d672fa338f98eda23877ced7ef237a9a422f8
Mar Garcia-Aloy <mar.garcia.aloy@gmail.com>
2021-04-02 17:58:23 +0200

    add MetaboAnalystR functions

---
+++ target/metaboanalyst.R
@@ -0,0 +1,23 @@
+library(MetaboAnalystR)
+mSet<-InitDataObjects("conc", "ts", FALSE)
+mSet<-SetDesignType(mSet, "time0")
+mSet<-Read.TextData(mSet, "C:/Users/garciaalom/Desktop/test.csv", "rowts", "disc")
+mSet<-SanityCheckData(mSet)
+#mSet<-ContainMissing(mSet)
+mSet<-ReplaceMin(mSet)
+mSet<-PreparePrenormData(mSet)
+mSet<-Normalization(mSet, "NULL", "LogNorm", "NULL", ratio=FALSE, ratioNum=20)
+#mSet<-PlotNormSummary(mSet, "norm_0_", "png", 72, width=NA)
+#mSet<-PlotSampleNormSummary(mSet, "snorm_0_", "png", 72, width=NA)
+mSet<-ANOVA2.Anal(mSet, 0.05, "fdr", "time0", 1, 1)
+tmp <- data.frame(mSet$analSet$aov2$sig.mat)
+tmp <- tmp[order(tmp$Adjusted.P.val)]
+head(tmp)
+PlotCmpdSummary(mSetObj = mSet, cmpdNm = "4.3148/653", #0, 
+                      format="png", dpi=72, width=NA)
+mSet <- performMB(mSet, 10)
+tmp <- data.frame(mSet$analSet$MB$stats)
+tmp$id <- rownames(tmp)
+tmp <- tmp[order(tmp$Hotelling.T2, decreasing = T),]
+head(tmp)
+PlotMBTimeProfile(mSet, "1.8927/853", "png", 72, width = NA)
